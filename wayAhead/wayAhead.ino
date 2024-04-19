533a68496f7df7f139806b51606bbee4b7be6b73
sipandrei <pontos.serban@gmail.com>
2024-04-19 22:23:06 +0300

    added display functions for all cases

--- wayAhead/wayAhead.ino
+++ wayAhead/wayAhead.ino
@@ -9,15 +9,18 @@ File recording;
 #include <Adafruit_SSD1306.h>
 #define SCREEN_HEIGHT 64
 #define SCREEN_WIDTH 128
-#define SCREEN_RESET -1
-#define SCREEN_ADDRESS 0x3C
+#define OLED_MOSI   9
+#define OLED_CLK   10
+#define OLED_DC    11
+#define OLED_CS    12
+#define OLED_RESET 13
 
-Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, SCREEN_RESET);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
 #include <TinyGPS++.h>
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
-#include <cmath>
+#include <math.h>
 
 static const int gpsRX = 4;
 static const int gpsTX = 3;
@@ -33,6 +36,7 @@ int currentScreen = 1;
 float oldLat,oldLon,oldAlt;
 float lati,lon,alt;
 float distance, averageSpeed;
+int startRecordTime = 0;
 
 #define SDCHIPSELECT 7
 String buffer;
@@ -99,6 +103,7 @@ void initializeRecording(String filename){
   distance = 0;
   averageSpeed = gps.speed.kmph();
   recording = SD.open(filename, FILE_WRITE);
+  startRecordTime = millis();  
 
   String header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<gpx creator=\"WayAheadGettoGPS\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd\" version=\"1.1\" xmlns=\"http://www.topografix.com/GPX/1/1\" xmlns:gpxtpx=\"http://www.garmin.com/xmlschemas/TrackPointExtension/v1\" xmlns:gpxx=\"http://www.garmin.com/xmlschemas/GpxExtensions/v3\">";
   header += "\n<metadata>";
@@ -122,7 +127,7 @@ void writeIfAvailable(){
 void finishRecording(){
   String footer = "\n</trkseg>\n</trk>\n</gpx>";
   addLineToBuffer(footer);
-  //afisare Saving...
+  savingScreen();
   while(buffer.length() != 0){
     writeIfAvailable();
   }
@@ -146,14 +151,18 @@ void buttonHandling(){
   if(selectButtonState = 1){
     switch(currentScreen/10){
       case 0:
-        isRecording = true;
-        oldRecordingState = false;
-        currentScreen = 10; //waiting for sattelites
+        if(currentScreen == 1){
+          isRecording = true;
+          oldRecordingState = false;
+          currentScreen = 10; //instant Infor screen
+          }
         break;
       case 1:
-        isRecording = false;
-        oldRecordingState = true;
-        currentScreen = 0; //saving screen
+        if(currentScreen == 12){
+          isRecording = false;
+          oldRecordingState = true;
+          currentScreen = 0; //saving screen
+        }
         break;
     }
   }
@@ -171,7 +180,7 @@ void buttonHandling(){
         if(currentScreen%10 < 3){
           currentScreen++;
         } else {
-          currentScreen = currentScreen/10*10+1;
+          currentScreen = currentScreen/10*10+0;
         }
         break;
     }
@@ -192,8 +201,8 @@ String nameMaker(){
 
 
 void updateDistance(){
-  float p2p = 2*6371*sqrt((1-cos(lati-oldLat)+cos(lati)*cos(oldLat)*(1-cos(lon-oldLon))/2)); //haversine equation
-  //float p2p = TinyGPSPlus::distanceBetween(lati,lon,oldLat,oldLon);
+  //float p2p = 2*6371*sqrt((1-cos(lati-oldLat)+cos(lati)*cos(oldLat)*(1-cos(lon-oldLon))/2)); //haversine equation
+  float p2p = TinyGPSPlus::distanceBetween(lati,lon,oldLat,oldLon);
   distance += p2p;
 }
 
@@ -221,16 +230,97 @@ void recordingHandling(){
   }
 }
 
+void savingScreen(){
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(display.width()/2-4,10);             // Start at top-left corner
+  display.println(F("Salvare Activitate. . ."));
+  display.display();
+  currentScreen = 1;
+}
+
+void startRecordingScreen(){
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(display.width()/2-4,10);
+  if(cycling){
+    display.println(F("• bicicleta"));
+    display.println(F("  alergare"));
+  } else {
+    display.println(F("  bicicleta"));
+    display.println(F("• alergare"));
+  }
+  display.display();
+}
+
+void stopScreen(){
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(display.width()/3-4,10);
+  display.println(F("Doresti sa salvezi?"));
+  display.display();
+}
+
+void rideInfo(float viteza){
+  int delta = (millis() - startRecordTime)*0.001;
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE); 
+  display.drawLine(display.width()-1, 10, display.width()+1, 10, SSD1306_WHITE);   
+  display.drawLine(display.width()-1, 20, display.width()+1, 20, SSD1306_WHITE);
+  display.drawLine(display.width()-1, 40, display.width()+1, 40, SSD1306_WHITE);  
+  display.setCursor(display.width()/3-4,5);
+  display.print(gps.satellites.value());
+  display.print("sat");
+  display.setCursor(display.width()/3-4,15);
+  display.print(delta/60/60); delta -= delta/60/60*60*60;
+  display.print(":");
+  display.print(delta/60); delta -= delta/60*60;
+  display.print(":");
+  display.println(delta);
+  display.setCursor(display.width()/2-4,35);
+  display.print(viteza);
+  display.println(F("km/h"));
+  display.setCursor(display.width()/2-4,50);
+  display.print(distance);
+  display.println(F("km"));
+  display.display();
+}
+
+void screenHandler(){
+  switch(currentScreen){
+    case 0:
+      savingScreen();
+    break;
+    case 1:
+      startRecordingScreen();
+    break;
+    case 11:
+      rideInfo(averageSpeed);
+    break;
+    case 12:
+      stopScreen();
+    break;
+    case 10:
+      rideInfo(gps.speed.kmph());
+    break; 
+  }
+}
+
 void setup() {
   Serial.begin(9600);
   buffer.reserve(512);    
-  if(!oled.begin(SCREEN_ADDRESS))
+  if(!display.begin(SSD1306_SWITCHCAPVCC))
     Serial.print("Screen not found!");
   if(!SD.begin(SDCHIPSELECT))
       Serial.println("SD Initialization failed");
   pinMode(selectButton, INPUT_PULLUP);
   pinMode(cycleButton, INPUT_PULLUP);
-
+  display.clearDisplay();
+  
   ss.begin(GPSBaud);
 }
 
@@ -248,5 +338,5 @@ void loop(){
   else if(oldRecordingState == true) {
     finishRecording();
   }
-  
+  screenHandler();
 }
